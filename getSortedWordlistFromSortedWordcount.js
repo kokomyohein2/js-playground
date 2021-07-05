466b77aba15f44c4f47e49dbd40463dce6735eac
Kokomyoheinhtet Unknown <kokomyoheinhtet@ascendcorp.com>
2021-07-05 13:49:13 +0700

    renamed file

--- getSortedWordlistFromSortedWordcount.js
+++ getSortedWordlistFromSortedWordcount.js
@@ -1,3 +1,6 @@
+// input : "hello","apple","apple", "book", "zoo", 3
+// output : "apple", "apple","book"
+
 const text =
   "Once you have the array, you could rebuild the object from the array in the order you like, thus achieving exactly what you set out to do. That would work in all the browsers I know of, but it would be dependent on an implementation quirk, and could break at any time. You should never make assumptions about the order of elements in a JavaScript object.";
 
